dec3cdd6dd84695445db82b2dc44653de0ab859b
elysahidayatur <124320545+elysahidayatur@users.noreply.github.com>
2024-05-08 11:27:15 +0700

    Add files via upload

---
+++ hasil.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hasil Perhitungan Kalkulator</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<form >
+		<h1>Hasil Perhitungan</h1>
+		
+		<?php 
+			$angka1 = $_GET['angka1'];
+			$angka2 = $_GET['angka2'];
+			$operator = $_GET['operator'];
+
+			if($operator == '+') {
+				$hasil = $angka1 + $angka2;
+			}else if($operator == '-') {
+				$hasil = $angka1 - $angka2;
+			}else if($operator == ':') {
+				$hasil = $angka1 / $angka2;
+			}else if($operator == 'x') {
+				$hasil = $angka1 * $angka2;
+			}
+			echo $angka1 . $operator . $angka2 . '=' . $hasil;
+
+		?>
+		<a href="index.php">Kembali</a>
+		<a type="button" onclick="return window.print();">Cetak</a>
+		
+	</form>
+
+</body>
+</html>
